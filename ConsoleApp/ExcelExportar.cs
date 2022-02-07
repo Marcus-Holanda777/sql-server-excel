db4b7c03813e317597bb339ffa238b68c22a36eb
Marcus Holanda <mvsh777@hotmail.com>
2022-02-07 00:09:27 -0300

    class exportar

---
+++ ConsoleApp/ExcelExportar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ClosedXML.Excel;
+using System.Data;
+
+namespace ConsoleApp
+{
+    class ExcelExportar
+    {
+        public string Caminho { get; set; }
+        public DataTable Tabela { get; set; }
+
+        public ExcelExportar(string caminho, DataTable tabela)
+        {
+            Caminho = caminho;
+            Tabela = tabela;
+        }
+        public void CriaExcel(string nomePlan)
+        {
+            using (XLWorkbook pasta = new XLWorkbook())
+            {
+                IXLWorksheet planilha = pasta.Worksheets.Add(nomePlan);
+
+                int col = 1;
+                foreach (DataColumn c in Tabela.Columns)
+                {
+                    planilha.Cell(1, col).Value = c.ColumnName;
+                    col++;
+                }
+
+                int linhas = 2;
+                foreach (DataRow linha in Tabela.Rows)
+                {
+                    int colunas = 1;
+                    foreach (DataColumn c in Tabela.Columns)
+                    {
+                        planilha.Cell(linhas, colunas).Value = linha[c.ColumnName];
+                        colunas++;
+                    }
+                    linhas++;
+                }
+
+                /* FORMATAR HEADER */
+                int ultima_coluna = Tabela.Columns.Count;
+                IXLCell inicio = planilha.Cell(1, 1);
+                IXLCell fim = planilha.Cell(1, ultima_coluna);
+
+                IXLRange cabeca = planilha.Range(inicio, fim);
+
+                cabeca.Style
+                    .Font.SetBold()
+                    .Font.SetFontColor(XLColor.White)
+                    .Fill.SetBackgroundColor(XLColor.BlueViolet)
+                    .Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+
+                planilha.SheetView.ZoomScale = 80;
+                planilha.Columns().AdjustToContents(); /* ajusta as colunas de forma automatica */
+                pasta.SaveAs(Caminho);
+            }
+        }
+    }
+}
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using ClosedXML.Excel;
 using System.Data;
 
 namespace ConsoleApp
@@ -8,57 +7,17 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Conexao conn = new Conexao("seu_servidor", "seu_banco");
+            Conexao conn = new Conexao("cosmos", "cosmos_v14b");
 
-            string query = "seu_select";
+            string query = "select top 100 * from kardex_filial";
+            string caminho = @"c:\arquivo\Kardex.xlsx";
+            string nomeTabela = "Kardex";
+            string nomePlan = "Resumo";
 
-            DataTable tbl = conn.Tabela("exemplo", query);
-            CriaExcel(@"caminho_do_excel", tbl);
-            
+            DataTable tbl = conn.Tabela(nomeTabela, query);
+            ExcelExportar excel = new ExcelExportar(caminho, tbl);
+            excel.CriaExcel(nomePlan);
         }
 
-        static void CriaExcel(string nome, DataTable dados)
-        {
-            using (XLWorkbook pasta = new XLWorkbook())
-            {
-                IXLWorksheet planilha = pasta.Worksheets.Add("nome_planilha");
-
-                int col = 1;
-                foreach(DataColumn c in dados.Columns)
-                {
-                    planilha.Cell(1, col).Value = c.ColumnName;
-                    col++;
-                }
-
-                int linhas = 2;
-                foreach (DataRow linha in dados.Rows)
-                {
-                    int colunas = 1;
-                    foreach (DataColumn c in dados.Columns)
-                    {
-                        planilha.Cell(linhas, colunas).Value = linha[c.ColumnName];
-                        colunas++;
-                    }
-                    linhas++;
-                }
-
-                /* FORMATAR HEADER */
-                int ultima_coluna = dados.Columns.Count;
-                IXLCell inicio = planilha.Cell(1, 1);
-                IXLCell fim = planilha.Cell(1, ultima_coluna);
-
-                IXLRange cabeca = planilha.Range(inicio, fim);
-
-                cabeca.Style
-                    .Font.SetBold()
-                    .Font.SetFontColor(XLColor.White)
-                    .Fill.SetBackgroundColor(XLColor.BlueViolet)
-                    .Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
-
-                planilha.SheetView.ZoomScale = 80;
-                planilha.Columns().AdjustToContents(); /* ajusta as colunas de forma automatica */
-                pasta.SaveAs(nome);
-            }
-        }
     }
 }
